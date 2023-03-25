7c58eb66773ccfe7b091e58c4e1d98a425b1fe1e
FunkyFr3sh <FunkyFr3sh@users.noreply.github.com>
2023-03-26 00:03:12 +0100

    Add SmoothScroll Plugin

---
+++ SmoothScroll/w2p.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include "patch.h"
+
+
+void __declspec(dllexport) w2p_init()
+{
+    /* Scroll 1 cell at a time rather than 2 cells */
+	PATCH_SET((char *)0x0043DEE9 + 1, "\x01");
+	PATCH_SET((char *)0x0043DEF4 + 1, "\x01");
+	PATCH_SET((char *)0x0043DF03 + 1, "\x01");
+	PATCH_SET((char *)0x0043DF0E + 1, "\x01");
+
+	/* Reduce delay between each "Scroll Tick" from 20ms to 10ms */
+	PATCH_SET((char *)0x0043DE8E + 2, "\x0A"); /* Mouse scroll */
+	PATCH_SET((char *)0x0043DC90 + 2, "\x0A"); /* Keyboard scroll */
+}
