e2fef83402d53a5b4b1d27442371e0dc06e6bc69
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-03-28 04:17:10 +0200

    allow fine tuning of scroll vars via .ini file

--- SmoothScroll/w2p.c
+++ SmoothScroll/w2p.c
@@ -5,13 +5,16 @@
 
 void __declspec(dllexport) w2p_init()
 {
-    /* Scroll 1 cell at a time rather than 2 cells */
-    PATCH_SET((char *)0x0043DEE9 + 1, "\x01");
-    PATCH_SET((char *)0x0043DEF4 + 1, "\x01");
-    PATCH_SET((char *)0x0043DF03 + 1, "\x01");
-    PATCH_SET((char *)0x0043DF0E + 1, "\x01");
+    BYTE delay = ini_get_int("Scroll", "Delay", 10, ".\\plugin\\SmoothScroll.ini");
+    BYTE amount = ini_get_int("Scroll", "Amount", 1, ".\\plugin\\SmoothScroll.ini");
 
-    /* Reduce delay between each "Scroll Tick" from 20ms to 10ms */
-    PATCH_SET((char *)0x0043DE8E + 2, "\x0A"); /* Mouse scroll */
-    PATCH_SET((char *)0x0043DC90 + 2, "\x0A"); /* Keyboard scroll */
+    /* Scroll x cells at a time rather than the default 2 cells */
+    patch_setbyte((char *)0x0043DEE9 + 1, amount);
+    patch_setbyte((char *)0x0043DEF4 + 1, amount);
+    patch_setbyte((char *)0x0043DF03 + 1, amount);
+    patch_setbyte((char *)0x0043DF0E + 1, amount);
+
+    /* Reduce delay between each "Scroll Tick" from 20ms to x ms */
+    patch_setbyte((char *)0x0043DE8E + 2, delay); /* Mouse scroll */
+    patch_setbyte((char *)0x0043DC90 + 2, delay); /* Keyboard scroll */
 }
