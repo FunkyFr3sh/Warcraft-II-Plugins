5a2cea49acb1e9a7bf0c86726fa61fcb391824da
FunkyFr3sh <FunkyFr3sh@users.noreply.github.com>
2023-05-11 23:51:53 +0200

    Update w2p.c

--- MMblack/w2p.c
+++ MMblack/w2p.c
@@ -5,8 +5,8 @@
 
 void __declspec(dllexport) w2p_init()
 {
-    BYTE paletteIndex = ini_get_int("MMblack", "PaletteIndex", 253, ".\\plugin\\MMblack.ini");
+    BYTE index = ini_get_int("MMblack", "PaletteIndex", 253, ".\\plugin\\MMblack.ini");
 
     /* Changes the mini map color for the black player */
-    patch_setbyte((char *)0x004A48B1, paletteIndex); /* Pink */
+    patch_setbyte((char *)0x004A48B1, index); /* Pink */
 }
