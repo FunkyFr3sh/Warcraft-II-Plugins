b28a2c3ec6a36568aa6173e536051887fe092260
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2025-01-10 07:30:21 +0100

    update patch.h to latest version

--- inc/patch.h
+++ inc/patch.h
@@ -1,28 +1,9 @@
+// LICENSE = BSD0 - https://github.com/FunkyFr3sh/petool
+
 #ifndef PATCH_H 
 #define PATCH_H 
 
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
-static inline PROC patch_call(char *src, char *dst)
-{
-    DWORD op = PAGE_EXECUTE_READ;
-    VirtualProtect(src, 5, PAGE_EXECUTE_READWRITE, &op);
-    src[0] = 0xE8;
-    DWORD org = *((DWORD *)(&src[1]));
-    *((DWORD *)(&src[1])) = dst - src - 5;
-    VirtualProtect(src, 5, op, &op);
-    return (PROC)(src + 5 + org);
-}
-
-static inline void patch_ljmp(char *src, char *dst)
-{
-    DWORD op = PAGE_EXECUTE_READ;
-    VirtualProtect(src, 5, PAGE_EXECUTE_READWRITE, &op);
-    src[0] = 0xE9;
-    *((DWORD *)(&src[1])) = dst - src - 5;
-    VirtualProtect(src, 5, op, &op);
-}
 
 static inline void patch_clear(char *start, char value, char *end)
 {
@@ -30,6 +11,45 @@ static inline void patch_clear(char *start, char value, char *end)
     VirtualProtect(start, end - start, PAGE_EXECUTE_READWRITE, &op);
     memset(start, value, end - start);
     VirtualProtect(start, end - start, op, &op);
+}
+
+static inline void patch_sjmp(char *src, char *dst)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(src, 2, PAGE_EXECUTE_READWRITE, &op);
+    src[0] = '\xEB';
+    src[1] = dst - src - 2;
+    VirtualProtect(src, 2, op, &op);
+}
+
+static inline void patch_ljmp(char *src, char *dst)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(src, 5, PAGE_EXECUTE_READWRITE, &op);
+    src[0] = '\xE9';
+    *((DWORD *)(&src[1])) = dst - src - 5;
+    VirtualProtect(src, 5, op, &op);
+}
+
+static inline PROC patch_call(char *src, char *dst)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(src, 5, PAGE_EXECUTE_READWRITE, &op);
+    src[0] = '\xE8';
+    DWORD org = *((DWORD *)(&src[1]));
+    *((DWORD *)(&src[1])) = dst - src - 5;
+    VirtualProtect(src, 5, op, &op);
+    return (PROC)(src + 5 + org);
+}
+
+static inline void patch_call_nop(char *src, char *dst)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(src, 6, PAGE_EXECUTE_READWRITE, &op);
+    src[0] = '\xE8';
+    *((DWORD *)(&src[1])) = dst - src - 5;
+    src[5] = '\x90';
+    VirtualProtect(src, 6, op, &op);
 }
 
 static inline DWORD patch_setdword(DWORD *dst, DWORD value)
@@ -70,6 +90,42 @@ static inline void patch_setbytes(char *dst, char *buf, size_t size)
     VirtualProtect(dst, size, op, &op);
 }
 
-#define PATCH_SET(a,b) patch_setbytes(a,b,sizeof(b)-1)
+#define PATCH_SET(a,b) patch_setbytes(a,(char*)b,sizeof(b)-1)
+
+static inline float patch_setfloat(float *dst, float value)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(dst, sizeof(float), PAGE_EXECUTE_READWRITE, &op);
+    float org = *dst;
+    *dst = value; 
+    VirtualProtect(dst, sizeof(float), op, &op);
+    return org;
+}
+
+static inline double patch_setdouble(double *dst, double value)
+{
+    DWORD op = PAGE_EXECUTE_READ;
+    VirtualProtect(dst, sizeof(double), PAGE_EXECUTE_READWRITE, &op);
+    double org = *dst;
+    *dst = value; 
+    VirtualProtect(dst, sizeof(double), op, &op);
+    return org;
+}
+
+static inline void patch_clear_nop(char *start, char *end)
+{
+    patch_clear(start, '\x90', end);
+}
+
+static inline void patch_clear_int(char *start, char *end)
+{
+    patch_clear(start, '\xCC', end);
+}
+
+static inline void patch_detour(char *start, char *end, char *dst)
+{
+    patch_clear(start + 5, '\xCC', end);
+    patch_ljmp(start, dst);
+}
 
 #endif
